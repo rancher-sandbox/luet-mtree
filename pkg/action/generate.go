ab9a585e0caaa5314288e973c899c9dfbf139b1a
Itxaka <igarcia@suse.com>
2021-05-21 12:19:04 +0200

    Add missing type keyword
    Signed-off-by: Itxaka <igarcia@suse.com>

--- pkg/action/generate.go
+++ pkg/action/generate.go
@@ -61,9 +61,7 @@ func (action generateAction) Run() error {
 	// Ignore time because luet tars files with the docker lib and that truncates the time to seconds only
 	// TODO(itxaka) we may be able to use tar_time ?
 	currentKeywords := []mtree.Keyword{
-		"mode",
-		"uid",
-		"gid",
+		"type",
 		"xattrs",
 		"sha512digest",
 	}
